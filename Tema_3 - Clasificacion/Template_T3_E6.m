8911a2fd017230c265002d7443f8a2e1bf6f89eb
sergiolo428 <sergiolo428@gmail.com>
2024-03-13 11:48:58 +0100

    T3E6 Updated

--- Tema_3 - Clasificacion/Template_T3_E6.m
+++ Tema_3 - Clasificacion/Template_T3_E6.m
@@ -68,7 +68,7 @@ X = Weekly{:,2:7};
 mdl = fitglm(X,Y,'Distribution','binomial','VarNames',var_names([var_sel end]))
 
 % Analizando el modelo podemos ver que el p-valor asociado al Chi^2 es
-% menor que 0.05 , es decir, hay lamenos un predictor que si tiene relacion
+% menor que 0.05 , es decir, hay almenos un predictor que si tiene relacion
 % con la variable a predecir
 
 % En este caso vemos que Lag2 tiene un p-valor de 0.029
@@ -79,8 +79,23 @@ disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
 % Apartado 3 - Calcula la matriz de confusión y el porcentaje de predicciones 
 % correctas. Examina la matriz de confusión y explica lo que ésta indica sobre 
 % los tipos de errores que el modelo de regresión logística comete.
+Yprob = predict(mdl,X);
 
+Ypred(Yprob>=0.5)=1;
+Ypred(Yprob<0.5)=0;
 
+C = confusionmat(Y,Ypred)
+confusionchart(C,{'Down (0)','Up (1)'})
+
+acc1 = 100*(C(1,2)+C(2,2))/length(Ypred)
+err1 = 100-acc1;
+
+% Fijandonos en la matriz, een la segunda columna veremos los valores
+% acertados, es decir los que originalmente en nuestro datos tenian un
+% valor y al predecir obtuvimos le mismo, en este caso 430 bajando, y 557
+% subienedo. Por otro lado vemos que en la prediccion 48 se han
+% categorizado como Down cuando en realidad eran Up, y 54 eran Down pero se
+% situaron como Up.
 
 fprintf('\n')
 disp('%%%%%%%%%%%%%%%%% Apartado 4 %%%%%%%%%%%%%%%%%');
@@ -90,7 +105,31 @@ disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
 % como único predictor. Calcula la matriz de confusión y el porcentaje de predicciones 
 % correctas para los datos de test (observaciones desde 2009 a 2010).
 
+pos_train2 = Weekly.Year<2008;
+pos_test2 = Weekly.Year>=2008;
 
+Xtrain2 = Weekly{pos_train2,3};
+Xtest2 = Weekly{pos_test2,3};
+
+Ytrain2 = Y(pos_train2);
+YtrueTest2 = Y(pos_test2);
+
+mdl = fitglm(Xtrain2,Ytrain2,'Distribution','binomial','VarNames',var_names([3 9]));
+
+Yprob2 = predict(mdl,Xtest2);
+
+Ypred2(Yprob2>=0.5)=0;
+Ypred2(Yprob2<0.5)=1;
+
+C = confusionmat(YtrueTest2,Ypred2)
+confusionchart(C,{'Down (0)','Up (1)'})
+
+acc2 = 100*(C(1,1)+C(2,1))/length(YtrueTest2)
+err2 = 100-acc2;
+
+% Podeemos observar como en este caso hemos obtenido un beneficio de un 2%
+% de acieerto, pasando de un 90% con todos los preditores a un 92% usando
+% unicamente Log2.
 
 fprintf('\n')
 disp('%%%%%%%%%%%%%%%%% Apartado 5 %%%%%%%%%%%%%%%%%');
