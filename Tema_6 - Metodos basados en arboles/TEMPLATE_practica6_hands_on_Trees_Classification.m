ad6c8fddbdcbc4c0501b07ff6f246204b3d55654
sergiolo428 <sergiolo428@gmail.com>
2024-04-18 17:35:13 +0200

    Updated T6

--- Tema_6 - Metodos basados en arboles/TEMPLATE_practica6_hands_on_Trees_Classification.m
+++ Tema_6 - Metodos basados en arboles/TEMPLATE_practica6_hands_on_Trees_Classification.m
@@ -13,14 +13,25 @@ disp('%%%%%%%%%%%%%%%%%% ÁRBOLES DE CLASIFICACIÓN %%%%%%%%%%%%%%%%%%%');
 disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
 
 % Creo variable dicotómica cualitativa High en base a la variable Sales
+High(Carseats.Sales>8) = {'Yes'};
+High(Carseats.Sales<=8) = {'No'};
+Y = High';
 
 
-% Ajustar árbol (probar Gini y entropía)
+% Ajustar Arbol (probar Gini y entropia)
+tree = fitctree(Carseats(:,2:end),Y,'CategoricalPredictors',[6,9,10],...
+    'SplitCriterion','gdi');
+
 
 % Predecir usando árbol generado
 
-% Tasas error y acierto
+label = predict(tree,Carseats(:,2:end));
 
+acierto = 100*sum(strcmp(label,Y))/length(Y);
+error = 100-acierto
+
+% Tasas error y acierto
+% view(tree,'Mode','graph')
 
 disp('%%%%%%%%%%%%% TODA LA BASE DE DATOS %%%%%%%%%%%%%')
 fprintf('Tasa de predicciones correctas = %4.2f%% \n',acierto);
@@ -35,10 +46,23 @@ fprintf('Tasa de error = %4.2f%% \n\n',error);
 % Dividimos, por tanto, la base de datos en train y test
 rng(5); % Fijamos semilla para el generado de números aleatorios
 
+hpartition = cvpartition(size(Carseats,1),'HoldOut',0.5);
+
+pos_train = hpartition.training;
+pos_test = hpartition.test;
+
+tree = fitctree(Carseats(pos_train,2:end),Y(pos_train),'CategoricalPredictors',[6,9,10],...
+    'SplitCriterion','gdi');
+
+alpha_grid = tree.PruneAlpha;
+
+label = predict(tree,Carseats(pos_test,2:end));
+
+acierto = 100*sum(strcmp(label,Y(pos_test)))/length(Y(pos_test));
+error = 100-acierto
+
 % Partición no estratificada
 % 50% train y 50% test
-
-
 
 
 % Entrenamos árbol de clasificación
@@ -59,39 +83,54 @@ fprintf('Tasa de predicciones correctas (TEST) = %4.2f%% \n\n',acierto);
 rng(2)
 k = 10;
 
+c = cvpartition(sum(pos_train),'KFold',10);
 
+X1 = Carseats(pos_train,2:end);
+Y1 = Y(pos_train);
 
 
 CV_error=[];
-% leafs=[];
 for aa = 1:k
     
-
-
-
+    pos_train_CV = c.training(aa);
+    pos_test_CV = c.test(aa);
     
+    xtrain = X1(pos_train_CV,:);
+    xtest = X1(pos_test_CV,:);
+
+    ytrain = Y1(pos_train_CV);
+    ytest = Y1(pos_test_CV);
+
+
+
     % Entrenamos árbol
-    
-    
+
+    tree_train = fitctree(xtrain,ytrain,'CategoricalPredictors',[6,9,10]);
+
     % Para cada alpha, ajustamos y evaluamos los modelos
     for bb=1:length(alpha_grid)-1 %Si hay M niveles de poda, hay M+1 alphas -> la última no cogemos sería poda completa -> decir clase mayoritaria
+        tree2 = prune(tree_train,'Alpha',alpha_grid(bb));
+        label = predict(tree2,xtest);
 
-
-
-
+        CV_error(aa,bb) = 100*(1-sum(strcmp(label,ytest))/length(ytest));
+        
     end
-    
 end
 
+[val,pos] = min(mean(CV_error))
+tree_pruned = prune(tree,'Alpha',alpha_grid(pos));
 
 % Visualizamos árbol de clasificación
 view(tree_pruned,'Mode','graph')
 
 % Evaluamos rendimiento del árbol de clasificación podado en test
-
-
+label = predict(tree_pruned,Carseats(pos_test,2:end));
+acierto = 100*sum(strcmp(label,Y(pos_test)))/length(Y(pos_test));
 fprintf('Tasa de predicciones correctas (TEST) del árbol podado (alpha=%.3f  nodos terminales=%d) = %4.2f%% \n\n',alpha_grid(pos),sum(~tree_pruned.IsBranchNode),acierto);
 
 % Dibujar error bar
 
+errorbar(alpha_grid(1:end-1),mean(CV_error),std(CV_error));
+hold on; plot(alpha_grid(1:end-1),mean(CV_error),'ro');hold off;
+xlabel('Alpha');ylabel('CV Error');
 
